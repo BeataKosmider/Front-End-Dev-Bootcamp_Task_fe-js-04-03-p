9576b2ba4703a0254bf733f018b790db73787e91
Beata Kosmider <kosmibe@gmail.com>
2023-02-04 10:13:01 +0100

    task with 1 test passed

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const getInputValue = (node) => {
+  const value = node?.getAttribute("value");
+
+  if (node?.hasAttribute("value")) {
+    return value;
+  } else {
+    return null;
+  }
+
   // Zaimplementuj zadanie zgodnie z wytycznymi
 };
 
